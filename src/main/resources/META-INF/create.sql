33fd21d143447f4d51e6a9a3faf564de1e3d94ce
CK2800 <cph-ck83@cphbusiness.dk>
2019-04-24 15:26:54 +0200

    Added drop-and-create strategy to pu_production.perperties

--- src/main/resources/META-INF/create.sql
+++ src/main/resources/META-INF/create.sql
@@ -1,5 +1,3 @@
-
-
 USE seed;
 INSERT INTO users (user_name, user_pass) VALUES ('user', '$2a$10$nYqwmiS8Mf.Y3DXEP36oVOpTf0Bv2IXVPwj8ah5CS7RRYAisCWexS');
 INSERT INTO users (user_name, user_pass) VALUES ('admin', '$2a$10$dAJcVYfNde9xVUExnZvjm./eksrvSKwjE0yEtGCOxwdVweJ4yqVvG');
